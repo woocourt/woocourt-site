fd6e299df67d8210091eaa07e34a474004b74982
jastructureit <josep.almolda@structureit.net>
2020-06-22 15:21:36 +0100

    add tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'woocourt-frontend';
+  selected = 0;
+
+  onTabClick(selectedTab: number) {
+    this.selected = selectedTab
+  }
 }
