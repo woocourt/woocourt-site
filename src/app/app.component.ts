2be79bcc47f5f963fce73b215873b78d25187a4f
jastructureit <josep.almolda@structureit.net>
2020-06-25 09:14:11 +0100

    change tabs styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'woocourt-frontend';
-  selected = 0;
+  title = 'woocourt-frontend'
+  selected = 0
 
   onTabClick(selectedTab: number) {
     this.selected = selectedTab
